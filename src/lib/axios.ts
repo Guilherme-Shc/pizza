add6dcdacb68d01298177eb177f3348fcd5cf2a7
Guilherme-Shc <antguischcor@gmail.com>
2024-05-08 15:29:11 -0300

    começando a conexão com a API

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,15 +4,15 @@ import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
-  withCredentials: true,
+  withCredentials: true, // used to send the frontend cookies to the backend, the profileKey in account-menu.tsx is an example
 })
 
-if (env.VITE_ENABLE_API_DELAY) {
-  api.interceptors.request.use(async (config) => {
-    await new Promise((resolve) =>
-      setTimeout(resolve, Math.round(Math.random() * 3000)),
-    )
+// if (env.VITE_ENABLE_API_DELAY) {
+//   api.interceptors.request.use(async (config) => {
+//     await new Promise((resolve) =>
+//       setTimeout(resolve, Math.round(Math.random() * 3000)),
+//     )
 
-    return config
-  })
-}
+//     return config
+//   })
+// }
